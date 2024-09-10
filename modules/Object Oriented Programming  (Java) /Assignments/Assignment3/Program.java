e569c28d1b5006daa90912796fac20b676dd632a
hardik-bhoir <hardikbhoir45@gmail.com>
2024-09-10 14:22:43 +0530

    Add files via upload

--- modules/Object Oriented Programming  (Java) /Assignments/Assignment3/Program.java
+++ modules/Object Oriented Programming  (Java) /Assignments/Assignment3/Program.java
@@ -2,33 +2,36 @@ package org.dac;
 
 import java.util.Scanner;
 
-class LoanAmortizationCalculator{
+class CompoundInterestCalculator{
 	private double principal;
-	private int loanterm;
-	private double annualinterest;
-	private double monthlypayment;
-	private double totalamount;
+	private double annualInterestRate;
+	private double years;
+	private double numberOfCompounds;
+	private double futureValue;
+	private double totalInterest;
 	public void acceptrecord (){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter principal");
 		this.principal = sc.nextDouble();
-		System.out.println("enter loan term");
-		this.loanterm = sc.nextInt();
-		System.out.println("enter annual interest");
-		this.annualinterest = sc.nextDouble();
+		System.out.println("get the annual interestn rate");
+		this.annualInterestRate = sc.nextDouble() / 100;
+		System.out.println("get the years");
+		this.years = sc.nextDouble();
+		System.out.println("get the number of compounds");
+		this.numberOfCompounds = sc.nextDouble();
 	}
 	
-	public void calculateMonthlyPayment() {
-		double monthlyinterest= annualinterest/12/100;
-		int numberOfMonths = loanterm*12;
-		this.monthlypayment = principal*(monthlyinterest*Math.pow(1+monthlyinterest,numberOfMonths))/(Math.pow(1+monthlyinterest , numberOfMonths)-1);
-		this.totalamount = monthlypayment*numberOfMonths;
-		
+	public void calculateFutureValue() {
+		double interest = annualInterestRate/numberOfCompounds;
+		double period = numberOfCompounds*years;
+		this.futureValue = principal*Math.pow((1+ interest),period);
+		this.totalInterest = futureValue-principal;
+	
 	}
-		
+	
 	public void printrecord() {
-		System.out.println("monthlypayment : "+this.monthlypayment);
-		System.out.println("total amount : "+this.totalamount);
+		System.out.println("future value is  : "+this.futureValue);
+		System.out.println("total interest is : "+this.totalInterest);
 	
 
 	}
@@ -37,9 +40,9 @@ class LoanAmortizationCalculator{
 
 public class Program {
 	public static void main(String[] args) {
-		LoanAmortizationCalculator cal = new LoanAmortizationCalculator();
+		CompoundInterestCalculator cal = new CompoundInterestCalculator();
 		cal.acceptrecord();
-		cal.calculateMonthlyPayment();
+		cal.calculateFutureValue();
 		cal.printrecord();
 		
 	}
